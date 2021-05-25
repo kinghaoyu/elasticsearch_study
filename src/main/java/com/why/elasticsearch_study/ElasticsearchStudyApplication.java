15608160917f1ca1bae1b09f1f75fa98e13bb956
wanghaoyu <wanghaoyu@mininglamp.com>
2021-05-25 16:20:34 +0800

    项目创建完成，添加postman_collections

---
+++ src/main/java/com/why/elasticsearch_study/ElasticsearchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.why.elasticsearch_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/why/elasticsearch_study/ElasticsearchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.why.elasticsearch_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
